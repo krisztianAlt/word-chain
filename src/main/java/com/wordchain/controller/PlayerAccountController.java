5ec0d9a86e29dadd0b9bf23056de493fc0f4f239
krisztian <alt.krisztian@gmail.com>
2018-01-31 12:12:32 +0100

    Create registration and login methods

--- src/main/java/com/wordchain/controller/PlayerAccountController.java
+++ src/main/java/com/wordchain/controller/PlayerAccountController.java
@@ -1,4 +1,37 @@
 package com.wordchain.controller;
 
+import com.wordchain.model.Player;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
 public class PlayerAccountController {
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("player", new Player());
+        model.addAttribute("errors", new ArrayList<>());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute Player player,
+                               @RequestParam("confirm") String confirm,
+                               Model model) {
+        model.addAttribute("player","SOMETHING");
+        model.addAttribute("errors", new ArrayList<>());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String checkWord(@RequestParam("word") String word,
+                            Model model){
+        return "dictionary-test";
+    }
 }
