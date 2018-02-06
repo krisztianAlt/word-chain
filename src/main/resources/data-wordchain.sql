952bcacefd669d73a9b7df50117fd2e35e453ded
krisztian <alt.krisztian@gmail.com>
2018-02-06 16:19:12 +0100

    Add second test user

--- src/main/resources/data-wordchain.sql
+++ src/main/resources/data-wordchain.sql
@@ -1,3 +1,4 @@
 INSERT INTO player VALUES (1, 'english@freemail.hu', 'USER', '$2a$10$8Kre7KpW8MXDqWJ3uh/a.eNiQAoeT0ONaZtg38UDm.Lz4U7o.LZSm', 'not_Shakespeare');
+INSERT INTO player VALUES (2, 'berci@freemail.hu', 'USER', '$2a$10$8Kre7KpW8MXDqWJ3uh/a.eNiQAoeT0ONaZtg38UDm.Lz4U7o.LZSm', 'Farkas_Bertalan');
 
-SELECT pg_catalog.setval('player_id_seq', 1, true);
+SELECT pg_catalog.setval('player_id_seq', 2, true);
