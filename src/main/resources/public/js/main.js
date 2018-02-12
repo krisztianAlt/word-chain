23b1784ba7a09aebeed23f343a214802dd527f29
krisztian <alt.krisztian@gmail.com>
2018-02-12 21:18:29 +0100

    Momently reroute My words button to / url

--- src/main/resources/public/js/main.js
+++
@@ -1,8 +0,0 @@
-var app = app || {};
-
-app.init = function() {
-    app.onlineEntitiesHandler.getOnlineIntitiesTimer();
-    app.onlineEntitiesHandler.newMatchButton();
-};
-
-$(document).ready(app.init());
